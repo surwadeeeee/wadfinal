d1fc1598a4a8b35e033e24f0b2f9fc1727056490
surwadeeeee <143114781+surwadeeeee@users.noreply.github.com>
2024-05-08 16:53:45 +0530

    Update app.component.ts

--- main angular/src/app/app.component.ts
+++ main angular/src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'proj1';
-  
+  title = 'Registrationform';
+
+  displayname='';
+  displayaddress='';
+  displaycontact='';
+  displayemail='';
+
+  getValue(name:string,address : string ,contact:string, email:string){
+    this.displayname = name;
+    this.displayaddress= address;
+    this.displaycontact=contact;
+    this.displayemail=email;
+  }
 }
+
 
